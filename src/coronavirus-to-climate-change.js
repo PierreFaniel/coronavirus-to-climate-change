169867bbb0584707c9780bd2d567f1309116cc2a
Pierre Faniel <pierre.faniel@alayacare.com>
2020-03-10 21:04:02 -0400

    Add support for french

--- src/coronavirus-to-climate-change.js
+++ src/coronavirus-to-climate-change.js
@@ -1,12 +1,24 @@
-const wordsToReplace = {
-  'Coronavirus': 'Climate Change',
-  'coronavirus': 'climate change', 
-  'corona virus': 'climate change', 
-  'COVID-19': 'Climate Change', 
-  'covid-19': 'climate change'
+const WORDS_TO_REPLACE = {
+  en: {
+    'Coronavirus': 'Climate Change',
+    'coronavirus': 'climate change', 
+    'corona virus': 'climate change', 
+    'COVID-19': 'Climate Change', 
+    'covid-19': 'climate change'
+  },
+  fr: {
+    'Coronavirus': 'Changement Climatique',
+    'coronavirus': 'changement climatique', 
+    'corona virus': 'changement climatique', 
+    'COVID-19': 'Changement Climatique', 
+    'covid-19': 'changement climatique'
+  }
 };
 
+const supportedLanguages = Object.keys(WORDS_TO_REPLACE);
+
 function replaceInText(element) {
+  const wordsToReplace = WORDS_TO_REPLACE[getLang()];
   for (let word in wordsToReplace) {
     element.nodeValue = element.nodeValue.replace(
       new RegExp(word, "ig"), 
@@ -36,10 +48,20 @@ function walkTextNodes(rootElement) {
   }
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-  console.log('CORONAHAHA');
-  setTimeout(() => {
+function getLang() {
+  if (
+      navigator.language && 
+      navigator.language.length >= 2 && 
+      supportedLanguages.includes(navigator.language.substring(0,2))
+    ) {
+      return navigator.language.substring(0, 2);
+  }
+  // Use en as default
+  return 'en';
+}
 
+document.addEventListener("DOMContentLoaded", function() {
+  setTimeout(() => {
     walkTextNodes(document.body);
   }, 100);
 });
